fb263b3ec97fd92c450f2091438b8aa875ef401a
Hamidreza-Fakharzadeh <hrfarzadeh@gmail.com>
2021-07-16 19:31:53 +0100

    style home page with sass

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import screen2 from '../images/screen2.png';
+import shahroz14 from '../images/shahroz14.jpg';
+import shahroz13 from '../images/shahroz13.jpg';
+import shahroz12 from '../images/shahroz12.jpg';
+import shahroz11 from '../images/shahroz11.jpg';
+import shahroz10 from '../images/shahroz10.jpg';
+import shahroz9 from '../images/shahroz9.jpg';
 import '../styles/MainPage.css';
+import '../styles/Footer.css';
+
 
 function Home() {
 	return (
-		<div className="mainPage">
-			<h1>Welcome to the Website</h1>
-			<img src={screen2} alt="img1" />
+		<div>
+			<div className="mainPage">
+				<h1>Welcome to</h1>
+				<div className="coName1">M&M</div>
+				<div className="coName2">CONSTRUCTION</div>
+				<div className="coName3">CO</div>
+				<div>
+					<img src={shahroz14} alt="img1" />
+				</div>
+				<div>
+					<img src={shahroz13} alt="img2" />
+				</div>
+				<div>
+					<img src={shahroz12} alt="img3" />
+				</div>
+				<div>
+					<img src={shahroz11} alt="img4" />
+				</div>
+				<div>
+					<img src={shahroz10} alt="img5" />
+				</div>
+				<div>
+					<img src={shahroz9} alt="img6" />
+				</div>
+			</div>
+			<div className="footer-wrapper">
+				<div className="footer-txt">FOOTER</div>
+			</div>
 		</div>
 	);
 }
