cec815ca63786c7a073d84647900c2519ad56011
Hamidreza-Fakharzadeh <hrfarzadeh@gmail.com>
2021-07-19 23:08:37 +0100

    add some images

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import shahroz14 from '../images/shahroz14.jpg';
-import shahroz13 from '../images/shahroz13.jpg';
+import shahroz1 from '../images/shahroz1.png';
+import shahroz2 from '../images/shahroz2.png';
 import shahroz12 from '../images/shahroz12.jpg';
 import shahroz11 from '../images/shahroz11.jpg';
-import shahroz10 from '../images/shahroz10.jpg';
-import shahroz9 from '../images/shahroz9.jpg';
+//import shahroz10 from '../images/shahroz10.jpg';
+//import shahroz9 from '../images/shahroz9.jpg';
 import '../styles/MainPage.css';
 import '../styles/Footer.css';
-
 
 function Home() {
 	return (
@@ -18,23 +17,23 @@ function Home() {
 				<div className="coName2">CONSTRUCTION</div>
 				<div className="coName3">CO</div>
 				<div>
-					<img src={shahroz14} alt="img1" />
+					<img src={shahroz1} alt="img1" />
 				</div>
 				<div>
-					<img src={shahroz13} alt="img2" />
-				</div>
-				<div>
-					<img src={shahroz12} alt="img3" />
+					<img src={shahroz2} alt="img5" />
 				</div>
 				<div>
 					<img src={shahroz11} alt="img4" />
 				</div>
 				<div>
-					<img src={shahroz10} alt="img5" />
+					<img src={shahroz12} alt="img3" />
 				</div>
-				<div>
+				{/* <div>
+					<img src={shahroz13} alt="img2" />
+				</div> */}
+				{/* <div>
 					<img src={shahroz9} alt="img6" />
-				</div>
+				</div> */}
 			</div>
 			<div className="footer-wrapper">
 				<div className="footer-txt">FOOTER</div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import shahroz9 from '../images/shahroz9.jpg';
 import '../styles/Navbar.css';
 function Navbar() {
 	return (
-		<div className="navbar">
-			<div className="links">
-				<Link to="/" className="navLink">
-					Home
-				</Link>
-				<Link to="/about" className="navLink">
-					About
-				</Link>
-				<Link to="/contact" className="navLink">
-					Contact
-				</Link>
+		<div className="header">
+			<div className="nav">
+				<img src={shahroz9.jpg} alt="logo" />
+				<ul>
+					<li>
+						<Link to="/" className="navLink">
+							Home
+						</Link>
+					</li>
+					<li>
+						<Link to="/about" className="navLink">
+							About
+						</Link>
+					</li>
+					<li>
+						<Link to="/contact" className="navLink">
+							Contact
+						</Link>
+					</li>
+				</ul>
 			</div>
 		</div>
 	);
